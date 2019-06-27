01cd1f6dbb78e67c11e67096686aefb5d7532353
Vanessa Araujo <vanessaraujs@gmail.com>
2019-06-26 23:15:44 -0300

    Criando Projeto

---
+++ src/main/java/com/spring/cobranca/CobrancaApplication.java
@@ -0,0 +1,15 @@
+package com.spring.cobranca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobrancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobrancaApplication.class, args);
+		
+		System.out.println("Logado!");
+	}
+
+}
