73cf70aac5e691b477af89b36bee366281f9b8d9
Vanessa Araujo <vanessaraujs@gmail.com>
2019-06-28 01:23:46 -0300

    Criando formulário de cadastro de títulos

--- src/main/java/com/spring/cobranca/controller/TituloController.java
+++ src/main/java/com/spring/cobranca/controller/TituloController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TituloController {
 	
-	@RequestMapping("/titulos/novo")
+	@RequestMapping("/titulos/Home")
 	public String novo() {
-		return "CadastroTitulo";
+		return "Home";
 	}
 }
